91c4aefe01bdd01f0b52157735aa652d55e1cd58
Maximiliano Gigena <maximiliano.gigena@outlook.com.ar>
2019-06-20 23:05:47 -0300

    Adding digest auth test

--- src/test/java/com/mgigena/automationdemo/pages/BasicPage.java
+++ src/test/java/com/mgigena/automationdemo/pages/BasicPage.java
@@ -3,7 +3,6 @@ package com.mgigena.automationdemo.pages;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasicPage {
